569f25ea7b9ebf6610f057331eb936ec093b75c9
Joaquin Mamani <42153383+sjmamani@users.noreply.github.com>
2021-04-04 20:09:50 -0300

    Agrega gráfico con dummy data

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
+
 function ShowHideMethod() {
-    var eulerDiv      = document.getElementById("eulerContent");
-    var montecarloDiv = document.getElementById("montecarloContent");
-    var isEuler       = document.getElementById("euler");
-    if (isEuler.checked) {
-        montecarloDiv.classList.add("hide");
-        eulerDiv.classList.remove("hide");
-    } else {
-        eulerDiv.classList.add("hide");
-        montecarloDiv.classList.remove("hide");
-    }
+    var eulerDiv = document.getElementById('eulerContent');
+	var montecarloDiv = document.getElementById('montecarloContent');
+    var isEuler = document.getElementById('euler');
+	if (isEuler.checked) {
+		montecarloDiv.classList.add('hide');
+		eulerDiv.classList.remove('hide');
+        plot('#eulerGraphic');
+	} else {
+		eulerDiv.classList.add('hide');
+		montecarloDiv.classList.remove('hide');
+        plot('#montecarloGraphic');
+	}
+}
+
+function plot(target = "#eulerGraphic") {
+	functionPlot({
+		target: target,
+		// width: 100,
+		// height: 100,
+		yAxis: { domain: [-1, 9] },
+		grid: true,
+		data: [
+			{
+				fn: 'x^2',
+				derivative: {
+					fn: '2 * x',
+					updateOnMouseMove: true,
+				},
+			},
+		],
+	});
 }
